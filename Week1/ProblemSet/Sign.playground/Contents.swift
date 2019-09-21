1a3632c15c5738f75054edcda205209dbfeb9df6
Elenore <elenore.nava@gmail.com>
2019-09-21 14:16:27 -0500

    Enum

--- Week1/ProblemSet/Sign.playground/Contents.swift
+++ Week1/ProblemSet/Sign.playground/Contents.swift
@@ -1,3 +1,50 @@
 import UIKit
+import GameplayKit
 
-var str = "Hello, playground"
+enum Sign: String {
+    case rock, paper, scissors
+    
+    var simpleDescription: String {
+        switch self {
+        case .rock:
+            return "👊🏻"
+        case .paper:
+            return "✋🏻"
+        case .scissors:
+            return "✌🏻"
+            
+        }
+    }
+
+
+//let rock = Sign.rock.simpleDescription
+//let paper = Sign.paper.simpleDescription
+//let scissors = Sign.scissors.simpleDescription
+
+func compare(computer: Sign) -> String {
+    if self == computer {
+        return "Empate"
+    }
+    switch self {
+    case .rock:
+        if computer == .paper{
+            return "lose"
+        } else if computer == .scissors {
+            return "win"
+        }
+    case .paper:
+        if computer == .scissors{
+            return "lose"
+        } else if computer == .rock {
+            return "win"
+        }
+    case .scissors:
+        if computer == .paper{
+            return "win"
+        } else if computer == .rock {
+            return "lose"
+        }
+    }
+    return ""
+}
+}
