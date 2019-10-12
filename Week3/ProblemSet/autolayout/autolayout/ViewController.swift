e5da04646589e8b66eba9cee2fbe7755fb609ec0
Elenore <elenore.nava@gmail.com>
2019-10-12 13:35:14 -0500

    trabajo clase 3

---
+++ Week3/ProblemSet/autolayout/autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autolayout
+//
+//  Created by Apple Device 11 on 10/12/19.
+//  Copyright © 2019 Apple Device 11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
