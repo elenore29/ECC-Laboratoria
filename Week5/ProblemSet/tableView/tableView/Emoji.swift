17cda0cf4ace8173e89d6190f37d5579a56f2bad
Elenore <elenore.nava@gmail.com>
2019-10-26 13:51:26 -0500

    Proyecto de emojis echo en clase

---
+++ Week5/ProblemSet/tableView/tableView/Emoji.swift
@@ -0,0 +1,16 @@
+//
+//  Emoji.swift
+//  tableView
+//
+//  Created by Apple Device 11 on 10/26/19.
+//  Copyright © 2019 Apple Device 11. All rights reserved.
+//
+
+import Foundation
+
+struct Emoji {
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
